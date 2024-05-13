84565be6410f5c7002391aecb489dda366c118ef
Mouhealdien <mhyaldynalamyn45@gmail.com>
2024-05-13 21:47:26 +0300

    edit hero text animation

--- src/components/global/HeroText.tsx
+++ src/components/global/HeroText.tsx
@@ -17,20 +17,20 @@ const HeroText = () => {
     }, 3000); // Change sentence every 3 seconds
 
     return () => clearInterval(interval);
-  }, [sentences.length]);
+  }, );
 
   const colorLastWord = (sentence: string) => {
     const words = sentence.split(" ");
     const lastWord = words[words.length - 1];
-    const coloredLastWord = `<span class="text-blue-600">${lastWord}</span>`;
+    const coloredLastWord = `<p class="text-blue-600 inline-block">${lastWord}</p>  `;
     const modifiedSentence = sentence.replace(lastWord, coloredLastWord);
-    return <span dangerouslySetInnerHTML={{ __html: modifiedSentence }} />;
+    return <p className="inline-block"  dangerouslySetInnerHTML={{ __html: modifiedSentence }} />;
   };
 
   return (
     <div className="flex items-center justify-center ">
       <div className=" mt-2 text-white  text-4xl p-4">
-        {sentences.map((sentence, index) => (
+        {/* {sentences.map((sentence, index) => (
           <div
             key={index}
             className={`absolute   md:top-20  lg:top-12 font-bold right-0 left-0 bottom-0 m-auto transition-all duration-1000 ${
@@ -39,7 +39,7 @@ const HeroText = () => {
                 : "opacity-0 translate-y-[-100%]"
             }`}
           >
-            <p className=" text-[23px] xs:text-3xl inline-block md:text-5xl">
+            <p className="  text-[23px] xs:text-3xl inline-block md:text-5xl">
               {colorLastWord(sentence)}
             </p>
             <svg
@@ -56,8 +56,43 @@ const HeroText = () => {
               />
             </svg>
           </div>
-        ))}
+        ))} */}
       </div>
+
+
+      
+      {/* <div className=" overflow-hidden h-[100px] pt-2  ">
+      <p className="textHero transition-all duration-1000 text-4xl">{sentences[currentIndex]}</p>
+      </div> */}
+
+     
+
+<div className="text-[23px] xs:text-3xl inline-block md:text-5xl">
+  <span className=" sentencesWrapper ">
+    {sentences.map((e,i)=>{
+        return <span className="heroSentence " key={i} >{colorLastWord(e)}
+        <svg
+        className="inline ml-0 pl-0 mt-[23px] md:mt-[27px]    w-[10px] h-[14px] md:w-[15px] md:h-[20px] "
+        width="15"
+        height="20"
+        viewBox="0 0 23 22"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M11.3813 -1.87188e-05L0.5 10.6636L11.3812 21.3272L22.2625 10.6636L11.3813 -1.87188e-05Z"
+          fill="#FFF"
+        />
+      </svg>
+        </span>
+    })}
+    
+   
+    </span>
+    
+</div>    
+
+
     </div>
   );
 };
